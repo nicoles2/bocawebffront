2ad9c58cfae25b898fca11d00bfdc9086624743e
nicoles2 <nicole.gomes@estudante.ifms.edu.br>
2022-09-22 15:17:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import { Cabecalho } from './components/Cabecalho';
+import { Rodapeodas } from './components/Rodapeodas';
+import { Busca } from './components/Busca';
+import { Lista } from './components/Lista';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    busca: '',
+    odas:[]
+  }
+  componentDidMount(){
+    this.carregaODAs();
+  }
+  componentDidUpdate(){
+    console.log(this.state)
+  }
+  carregaODAs(){
+    const {busca}=this.state;
+    fetch('https://www.bocaweb.com.br/apibocaweb?nome='+busca)
+    .then(response => response.json())
+    .then(odas => this.setState({odas}))
+    console.log({busca})
+  }
+  buscaODA=(evento) => {
+    this.setState({busca: evento.target.value});
+    this.carregaODAs()
+  }
+
+  render(){
+    const {odas}=this.state;
+    return(
+      <section className ="container">  
+            <div className="cabecalho">
+              <Cabecalho/>
+            </div>
+            
+              <Busca
+                busca={this.state.busca}
+                buscaODA={this.buscaODA}
+              />
+           
+              <Lista 
+                Lista = {odas}
+              />
+         
+            
+              <Rodapeodas/>
+            
+      </section>
+    )
+  }
 }
 
 export default App;
